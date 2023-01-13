6bb806ef6249e1f760a817d0ddf20bc218acfc4b
K18Tatiana <tatianavegac2003@gmail.com>
2023-01-13 14:42:45 -0500

    sucess

---
+++ src/components/Success.jsx
@@ -0,0 +1,11 @@
+const Sucess = (  ) => {
+
+    return (
+        <div className="success">
+            <h2>El producto fue creado con exito</h2>
+            <button>Cerrar</button>
+        </div>
+    )
+}
+
+export default Sucess
