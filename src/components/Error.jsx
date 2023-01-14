fbec3aeed06cb44baa00b88c2e7bf8893a4cb361
Flavio Gonzalo Villalba Gargantini <gonzagargantini20@gmail.com>
2023-01-14 18:23:28 -0300

    cssErroFin

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,8 +1,13 @@
 import '/src/assets/styles/Error.css'
+import error from '../assets/img/error.png'
 const Error = ({operationType}) => {
 return(
-    <div className="error">
-        <h4>¡{`${operationType}`} FALLIDA!</h4>
+    <div className="conteiner">
+        <div className='conteiner__error'>
+            <img src={error} alt="" />
+            <h4 >¡{`${operationType}`} FALLIDA!</h4>
+        </div>
+        
     </div>
 )
 }
